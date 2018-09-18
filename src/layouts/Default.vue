e961f94e50cee823adfdfdcfd7539a4bdb92b620
Tommy Vedvik <tommyvedvik@gmail.com>
2018-09-18 08:48:45 +0200

    Added example image to index

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -15,19 +15,27 @@
 
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif
+  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+  margin:0;
+  padding:0;
+  line-height: 1.5;
 }
+
 .layout {
   max-width: 600px;
   margin: 0 auto;
   padding-left: 20px;
   padding-right: 20px;
 }
+
 .header {
   display: flex;
   justify-content: space-between;
+  align-items: center;
   margin-bottom: 20px;
+  height: 80px;
 }
+
 .nav__link {
   margin-left: 20px;
 }
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,8 @@
 <template>
   <Layout>
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
     <h1>Hello, world!</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores ab harum aliquid dignissimos neque.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores</p>
   </Layout>
 </template>
 
