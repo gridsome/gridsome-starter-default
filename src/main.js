7d4f10322bc8d8a099671da144b54c82a2d19d74
Tommy Vedvik <tommyvedvik@gmail.com>
2019-01-07 20:19:32 +0100

    add variables to main.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 
 import DefaultLayout from '~/layouts/Default.vue'
 
-export default function (Vue) {
+export default function (Vue, {router, head, isClient}) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
