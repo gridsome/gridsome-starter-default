a8bf4c15b064370447ad4ac52b1afd85f488aac2
Tommy Vedvik <tommyvedvik@gmail.com>
2019-01-10 11:12:24 +0100

    chore: Add restart server inline docs

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,6 +1,9 @@
 // This is where project configuration and installed plugin options are located. 
 // Learn more: https://gridsome.org/docs/config
 
+// Changes here requires a server restart to work properly.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
 module.exports = {
   siteName: 'Gridsome',
   plugins: []
