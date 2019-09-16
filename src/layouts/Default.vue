317047a423ee10e7b5ef0c7bcf02e722cef04512
Hans-Jørgen Vedvik <hjvedvik@gmail.com>
2019-09-16 23:12:17 +0200

    update to gridsome@0.7.0

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,11 +2,11 @@
   <div class="layout">
     <header class="header">
       <strong>
-        <g-link to="/">{{ $static.metaData.siteName }}</g-link>
+        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
       </strong>
       <nav class="nav">
         <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about">About</g-link>
+        <g-link class="nav__link" to="/about/">About</g-link>
       </nav>
     </header>
     <slot/>
@@ -15,7 +15,7 @@
 
 <static-query>
 query {
-  metaData {
+  metadata {
     siteName
   }
 }
